b2d84a55ba8a1db2de0ecddb541ece6b5d038b26
Eduardaskuliesa <eduardas2000@mail.ru>
2025-11-19 17:14:35 +0200

    feat: remove unused S3 bucket configuration and add .env.example file

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const config = {
-  s3Bucket: {
-    name: process.env.AWS_S3_BUCKET_NAME,
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
   supabase: {
     url: process.env.SUPABASE_URL!,
     key: process.env.SUPABASE_SERVICE_ROLE_KEY!,
